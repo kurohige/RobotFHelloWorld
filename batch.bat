b5de41a98f16372fc60a8038949e484a0d481d3b
Kurohige <jhidalgopalacios@yahoo.com>
2022-07-06 10:13:46 +0300

    update to yaml

--- batch.bat
+++ batch.bat
@@ -1,3 +1,5 @@
 @echo off
-cd D:\Etteplan academy\RobotPractice
-call robot -d Results -v BROWSER:firefox Tests/amazon.robot
+:: call robot -d Results -v BROWSER:firefox Tests/amazon.robot
+call git add --all
+call git commit -m "update to yaml"
+call push
